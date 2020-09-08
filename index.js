fcb64cb3202ffc3c1cb5e9e1719d20d9fd14a1ca
Jasmin Landry <jasmin.landry@hotmail.com>
2020-09-08 14:49:49 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+const axios = require('axios');
+
+async function getBugcrowdPrograms() {
+    let offsetArray = [];
+    for (let i = 0; i <= 350; i += 25) {
+        offsetArray.push(i)
+    }
+    let programList = [];
+    for (let offset of offsetArray) {
+            let response = await axios.request({
+            url: `https://bugcrowd.com/programs.json?sort[]=name-asc&hidden[]=false&offset[]=${offset}`,
+            method: 'get',
+            headers: {
+                Cookie: `_crowdcontrol_session=${process.env.BUGCROWD_COOKIE}`
+            }
+        });
+        var programs = response.data.programs;
+        programs.map(program => {
+            programList.push(program.name)
+        })
+    }
+    console.dir(programList.sort(), { 'maxArrayLength': null });
+};
+
+const start = async function () {
+    await getBugcrowdPrograms();
+}
+
+if (validation()) {
+    start();
+  }
+
+function validation() {
+    if (!process.env.BUGCROWD_COOKIE) {
+      console.error("Missing Bugcrowd Session Cookie");
+      return false
+    }
+    return true
+};
