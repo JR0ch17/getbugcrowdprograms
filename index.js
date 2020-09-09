17ec9433dc72c87b0cdad3c6ba6b84d9b142945d
Jasmin Landry <jasmin.landry@hotmail.com>
2020-09-08 22:17:12 -0400

    Dynamically look up amount of programs

--- index.js
+++ index.js
@@ -2,20 +2,29 @@
 const axios = require('axios');
 
 async function getBugcrowdPrograms() {
+    let programsResponse = await axios.request({
+        url: `https://bugcrowd.com/programs.json`,
+        method: 'get',
+        headers: {
+            Cookie: `_crowdcontrol_session=${process.env.BUGCROWD_COOKIE}`
+        }
+    });
+    let programsAmount = programsResponse.data.meta.quickFilterCounts.all;
+
     let offsetArray = [];
-    for (let i = 0; i <= 350; i += 25) {
+    for (let i = 0; i <= programsAmount; i += 25) {
         offsetArray.push(i)
     }
     let programList = [];
     for (let offset of offsetArray) {
-            let response = await axios.request({
+        let response = await axios.request({
             url: `https://bugcrowd.com/programs.json?sort[]=name-asc&hidden[]=false&offset[]=${offset}`,
             method: 'get',
             headers: {
                 Cookie: `_crowdcontrol_session=${process.env.BUGCROWD_COOKIE}`
             }
         });
-        var programs = response.data.programs;
+        let programs = response.data.programs;
         programs.map(program => {
             programList.push(program.name)
         })
@@ -32,12 +41,12 @@ const start = async function () {
 
 if (validation()) {
     start();
-  }
+}
 
 function validation() {
     if (!process.env.BUGCROWD_COOKIE) {
-      console.error("Missing Bugcrowd Session Cookie");
-      return false
+        console.error("Missing Bugcrowd Session Cookie");
+        return false
     }
     return true
 };
